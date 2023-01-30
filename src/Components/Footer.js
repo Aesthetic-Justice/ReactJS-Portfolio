4011c118fd7fbecf7ddf7912ff91f6ea2c91a041
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2023-01-30 17:55:54 -0500

    Anchor footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer" style={{position:"fixed",bottom: 0, right:0,width:"100rem"}}>
       <div className="has-text-centered">
         <h4>
           This portfolio was created under the <a href="https://opensource.org/licenses/MIT">MIT License</a>. Please refer to the official online resource for further detail.
