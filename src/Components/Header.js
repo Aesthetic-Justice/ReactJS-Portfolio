87e7a8ed1e624b440d30b3fdbc3e05a5fdb134aa
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2023-01-31 13:41:00 -0500

    Added Nav to Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,18 @@
 import 'bulma/css/bulma.min.css';
+import Nagivation from './Navigation';
 
 const App = () => {
   return (
-    <section class="hero is-dark is-small">
-      <div class="hero-body">
-        <div class="columns is-mobile is-centered">
-          <div class="column is-1">Test</div>
-          <div class="column is-1">Test</div>
-          <div class="column is-1">Test</div>
+    <section class="hero is-dark">
+      <div class="hero-body" style={{ width: "100vw" }}>
+        <div>
+          <a href="./">
+            <img alt='Site Logo' src="./nameSig.png" />
+          </a>
+          <Nagivation />
         </div>
       </div>
     </section>
-
   )
 }
 
