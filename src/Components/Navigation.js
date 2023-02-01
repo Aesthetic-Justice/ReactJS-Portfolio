0e89e06018e2652941379732e60f8711c045a2d3
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2023-02-01 01:51:16 -0500

    NavBar now highlights when clicked

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,36 +2,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-/*     const NavBar = document.getElementsByClassName('navbar-brand')[0];
-    switch (window.location.pathname) {
-        case "/contact-me":
-            NavBar.children[3].children[0].classList.add('is-outlined');
-            break;
-        default:
-            console.log(window.location.pathname)
-            break;
-    } */
+
+    const handleClick = event => {
+        let NavBarButtons = document.getElementsByClassName('button')
+
+        if(event.target.className==='column'){
+            event.target=event.target.parentNode;
+        } else {
+            return;
+        }
+
+        NavBarButtons[0].className='button is-info';
+        NavBarButtons[1].className='button is-info';
+        NavBarButtons[2].className='button is-info';
+        NavBarButtons[3].className='button is-info';
+
+        event.target.className='button is-info is-outlined';
+    }
 
     return (
         <nav className="navbar subtitle is-6 is-justify-content-right">
             <ul className="navbar-brand columns is-vcentered is-multiline" >
                 <li className='mr-2'>
-                    <button className='button is-info is-outlined'>
+                    <button className='button is-info is-outlined' onClick={handleClick}>
                         <Link className="column" to="/">About Me</Link>
                     </button>
                 </li>
                 <li className='mr-2'>
-                    <button className='button is-info'>
+                    <button className='button is-info' onClick={handleClick}>
                         <Link className="column" to="/projects">Projects</Link>
                     </button>
                 </li>
                 <li className='mr-2'>
-                    <button className='button is-info'>
+                    <button className='button is-info' onClick={handleClick}>
                         <Link className="column" to="/resume">Resume</Link>
                     </button>
                 </li>
                 <li className='mr-2'>
-                    <button className='button is-info'>
+                    <button className='button is-info' onClick={handleClick}>
                         <Link className="column" to="/contact-me">Contact Me</Link>
                     </button>
                 </li>
